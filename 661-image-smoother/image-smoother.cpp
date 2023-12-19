ea1745e6ba335debe61a08d8249ac180f2122824
Baibhav Singh <baibhavsingh22@gmail.com>
2023-12-19 11:30:29 +0530

    Time: 45 ms (47.08%) | Memory: 22 MB (92.62%) - LeetSync

---
+++ 661-image-smoother/image-smoother.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& a) {
+        int i,j,k,c=0,s=0;
+        int n=a.size();
+        int m = a[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,0));
+
+        for(i=0;i<n;i++){
+            
+            for(j=0;j<m;j++){
+                c=0;
+                s=1;
+                c=a[i][j];
+                c+=i-1>=0 ? a[i-1][j]:0;
+                c+=i+1<n ? a[i+1][j]:0;
+                c+=j-1>=0 ? a[i][j-1]:0;
+                c+=j+1<m ? a[i][j+1]:0;
+                c+= i+1<n and  j+1<m ? a[i+1][j+1]:0;
+                c+= i+1<n and  j-1>=0 ? a[i+1][j-1]:0;
+                c+= i-1>=0 and  j+1<m ? a[i-1][j+1]:0;
+                c+= i-1>=0 and  j-1>=0 ? a[i-1][j-1]:0;
+
+                if(i-1>=0)
+                s++;
+                if(j-1>=0)
+                s++;
+                if(i+1<n)
+                s++;
+                if(j+1<m)
+                s++;
+                if(i-1>=0 and j-1>=0)
+                s++;
+                if(i+1<n and j+1<m)
+                s++;
+                if(i+1<n and j-1>=0)
+                s++;
+                if(i-1>=0 and j+1<m)
+                s++;
+                
+
+            ans[i][j]=c/s;
+                
+            }
+        }
+
+        return ans;
+    }
+};
