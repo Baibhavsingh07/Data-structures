e7c79bb747fca2509f7383e0d04ed9e4d779e554
Baibhav Singh <baibhavsingh22@gmail.com>
2023-12-21 12:24:53 +0530

    Time: 124 ms (55.58%) | Memory: 83.9 MB (72.54%) - LeetSync

--- 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
+++ 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
@@ -4,42 +4,24 @@ public:
 vector<vector<int>>tbl;
 // map<vector<int>,int>tbl;
 
-    int f(int i,int j,vector<int>& a, vector<int>& b){ //a - multiplier , b-nums
-        if(i==a.size())
-        return 0;
-
-    
-
-        int n=b.size();
-
-        if(tbl[i][j]!=INT_MIN)
-        return tbl[i][j];
-
-        
-
-        int k = n - i+j-1;   //calculating index from last
-
-        if(j>k)
-        return 0;
-
-        int l = a[i]*b[j]+f(i+1,j+1,a,b);
-        int r = a[i]*b[k]+f(i+1,j,a,b);
-
-        tbl[i][j] = max(l,r);
-        
-
-        return tbl[i][j];
-    }
-
     int maximumScore(vector<int>& a, vector<int>& b) {
-        tbl.resize(b.size()+1,vector<int>(b.size()+1,INT_MIN));
+        tbl.resize(b.size()+1,vector<int>(b.size()+1,0));
 
-        // for(int i=b.size();i>=0;i--){
-        //     for(int j=0;j<b.size();j++){
+        for(int i=b.size()-1;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int n=a.size();
+                int k = n - i+j-1;   //calculating index from last
+                
+                int l = b[i]*a[j]+tbl[i+1][j+1];
+                int r = b[i]*a[k]+tbl[i+1][j];
+                
+                tbl[i][j] = max(l,r);
 
-        //     }
-        // }
+            }
+        }
 
-        return f(0,0,b,a);
+        return tbl[0][0];
+
+        // return f(0,0,a,b);
     }
 };
