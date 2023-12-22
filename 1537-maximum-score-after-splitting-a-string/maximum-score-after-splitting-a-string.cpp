891d445eb7f45cf3473007f7081531c0078ac3cf
Baibhav Singh <baibhavsingh22@gmail.com>
2023-12-22 17:44:50 +0530

    Time: 6 ms (15.62%) | Memory: 6.7 MB (43.75%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int i,j,c=0,ones=0,z=0;
+        int i,j,c=0;
 
         for(i=1;i<s.size();i++){
-            z=0;
-            ones=0;
+            int zeroes=0,ones=0;
             for(j=0;j<i;j++)
             if(s[j]=='0')
-            z++;
+            zeroes++;
 
             for(j=i;j<s.size();j++)
             if(s[j]=='1')
             ones++;
 
-            c=max(c,ones+z);
+            c=max(c,ones+zeroes);
         }
 
         return c;
