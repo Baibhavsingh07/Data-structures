7bc7895c14011be463f0061fd497e40793780383
Baibhav Singh <baibhavsingh22@gmail.com>
2023-12-26 11:30:19 +0530

    Time: 7 ms (83.81%) | Memory: 9.4 MB (46.24%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,40 +4,30 @@ public:
     // map<pair<int,int>,int>tbl;
     vector<vector<int>>tbl;
 
-    int f(int i,int j,string &a,string& b){
-        if(j==b.size())
-        if(i==a.size())
-        return 0;
-        else
-        return a.size()-i;
+    int f(int n,int m,string &a,string& b){
+        if(n==0) return m;
+        if(m==0) return n;
 
-        if(i==a.size())
-        return b.size()-j;
+        int match=INT_MAX-1,replace=INT_MAX-1,del = INT_MAX-1,insert = INT_MAX-1;
 
-        if(tbl[i][j]==-1){
+        if(tbl[n][m]!=-1) return tbl[n][m];
 
-        int l=INT_MAX,r=INT_MAX,p=INT_MAX;
+        if(a[n-1]==b[m-1]){
+            match = f(n-1,m-1,a,b);
+        }else{
+
+        replace = 1+f(n-1,m-1,a,b);
+        del = 1+f(n-1,m,a,b);
+        insert = 1+ f(n,m-1,a,b);
+        }
         
-        if(a[i]!=b[j]){
-            l=1 + f(i+1,j,a,b) == INT_MAX ? INT_MAX-1:1 + f(i+1,j,a,b);
-            r = 1+ f(i+1,j+1,a,b);
-            p= 1+ f(i,j+1,a,b);
-        } else{
-            l=f(i+1,j+1,a,b);
 
-        }
-        tbl[i][j] = min(p,min(l,r));
-        }
-
-        // if(l==r and r==p and p==INT_MAX)
-        // return INT_MAX;
-
-        return tbl[i][j];
+        return tbl[n][m] = min(insert,min(replace,min(match,del)));
 
     }
 
     int minDistance(string a, string b) {
         tbl.resize(a.size()+1,vector<int>(b.size()+1,-1));
-        return f(0,0,a,b);
+        return f(a.size(),b.size(),a,b);
     }
 };
