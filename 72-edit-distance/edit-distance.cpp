5ec320825477bcc28cfc5663896309e5f6203b11
Baibhav Singh <baibhavsingh22@gmail.com>
2023-12-26 11:58:20 +0530

    Time: 9 ms (67.04%) | Memory: 9.3 MB (68.19%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,28 +4,6 @@ public:
     // map<pair<int,int>,int>tbl;
     vector<vector<int>>tbl;
 
-    int f(int n,int m,string &a,string& b){
-        if(n==0) return m;
-        if(m==0) return n;
-
-        int match=INT_MAX-1,replace=INT_MAX-1,del = INT_MAX-1,insert = INT_MAX-1;
-
-        if(tbl[n][m]!=-1) return tbl[n][m];
-
-        if(a[n-1]==b[m-1]){
-            match = f(n-1,m-1,a,b);
-        }else{
-
-        replace = 1+f(n-1,m-1,a,b);
-        del = 1+f(n-1,m,a,b);
-        insert = 1+ f(n,m-1,a,b);
-        }
-        
-
-        return tbl[n][m] = min(insert,min(replace,min(match,del)));
-
-    }
-
     int minDistance(string a, string b) {
         tbl.resize(a.size()+1,vector<int>(b.size()+1,0));
 
