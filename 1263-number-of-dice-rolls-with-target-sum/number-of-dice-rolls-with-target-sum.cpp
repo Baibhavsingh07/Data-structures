400e87194792578339cfc65e9c16ca2be7612979
Baibhav Singh <baibhavsingh22@gmail.com>
2023-12-26 09:40:47 +0530

    Time: 1729 ms (5.29%) | Memory: 156.5 MB (5.21%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -3,29 +3,27 @@ public:
 
 int mod = 1000000007;
 
-// map<vector<int>,int>tbl;
-vector<vector<int>>tbl;
+map<vector<int>,int>tbl;
 
     int f(int i,int n,int k,int t){
         if(i==n and t==0) return 1;
         if(i==n or t<0 ) return 0;
 
-       int ans=0;
+        int ans=0;
 
-        if(tbl[i][t]==-1){
+        if(tbl.find({i,t})==tbl.end()){
         for(int j=1;j<=k;j++){
             ans=(ans + (f(i+1,n,k,t-j)%mod))%mod;
         }
 
-        tbl[i][t] =  (ans)%mod;
+        tbl[{i,t}] =  ans;
         }
 
-        return (tbl[i][t])%mod;
+        return tbl[{i,t}];
 
     }
 
     int numRollsToTarget(int n, int k, int t) {
-        tbl.resize(n+1,vector<int>(t+1,-1));
         return f(0,n,k,t);
     }
 };
