ed4592c013027ba85f938823246f2829be4765e7
Baibhav Singh <baibhavsingh22@gmail.com>
2023-12-31 09:06:49 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (41.15%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -3,13 +3,17 @@ public:
     int maxLengthBetweenEqualCharacters(string s) {
         int i,j,c=-1;
 
+        unordered_map<char,int>map;
+
         for(i=0;i<s.size();i++){
-            for(j=i+1;j<s.size();j++){
-                if(s[i]==s[j]){
-                    c=max(c,j-i-1);
-                }
+            if(map.find(s[i])==map.end())
+            map[s[i]]=i;
+            else{
+                int x = map[s[i]];
+                c = max(c,i-x-1);
             }
         }
+
         return c;
     }
 };
