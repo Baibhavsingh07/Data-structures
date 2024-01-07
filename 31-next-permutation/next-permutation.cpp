929c4503b4774092050402e04f099ee9dae08424
Baibhav Singh <baibhavsingh22@gmail.com>
2024-01-07 18:58:18 +0530

    Time: 4 ms (56.47%) | Memory: 12.5 MB (22.33%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& a) {
+        int i,j,k,c=0;
+	
+	for(i=a.size()-1;i>0;i--){
+		if(a[i]>a[i-1]){
+			c=1;
+			break;
+		}
+	}
+	
+	if(c==0) {
+		sort(a.begin(),a.end());
+		return;
+	}
+	
+	j=i;
+	k=i;
+	int m = a[i];
+	for(j=i;j<a.size();j++){
+		if(a[j]>a[i-1] and a[j]<m){
+			
+			k=j;
+			m=a[j];
+	}
+	}
+	
+	int t = a[k];
+	a[k]=a[i-1];
+	a[i-1]=t;
+	
+	sort(a.begin()+i,a.end());
+    }
+};
