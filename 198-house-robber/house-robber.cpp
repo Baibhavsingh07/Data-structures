6a1b5c78533d2dbec83da208db9dd74b7b453f99
Baibhav Singh <baibhavsingh22@gmail.com>
2024-01-21 21:06:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,18 +3,23 @@ public:
 
 
 
-    int f(int i,vector<int>&a,vector<int>&tbl){
-        if(i>=a.size()) return 0;
+    // int f(int i,vector<int>&a,vector<int>&tbl){
+    //     if(i>=a.size()) return 0;
 
-        if(tbl[i]!=-1) return tbl[i];
+    //     if(tbl[i]!=-1) return tbl[i];
 
-        return tbl[i] =  max(f(i+1,a,tbl),a[i]+f(i+2,a,tbl));
-    }
+    //     return tbl[i] =  max(f(i+1,a,tbl),a[i]+f(i+2,a,tbl));
+    // }
  
     int rob(vector<int>& a) { 
         
         // unordered_map<int,int>tbl;
-        vector<int>tbl(a.size(),-1);
-        return f(0,a,tbl);
+        vector<int>tbl(a.size()+2,0);
+
+        for(int i=a.size()-1;i>=0;i--){
+            tbl[i] = max(tbl[i+1],a[i]+tbl[i+2]);
+        }
+
+        return tbl[0];
     }
 };
