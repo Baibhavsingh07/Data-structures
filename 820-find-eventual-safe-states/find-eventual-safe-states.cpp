700c79ca07867220e18036b77cde62a93bb351ce
Baibhav Singh <baibhavsingh22@gmail.com>
2024-02-15 21:16:46 +0530

    Time: 147 ms (53.09%) | Memory: 65.7 MB (11.58%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,37 +1,65 @@
 class Solution {
 public:
 
-    int dfs(int i,vector<vector<int>>&a,vector<int>&v,vector<int>&ans,vector<int>&pv){
-        v[i]=1;
-        pv[i]=1;
+    // int dfs(int i,vector<vector<int>>&a,vector<int>&v,vector<int>&ans,vector<int>&pv){
+    //     v[i]=1;
+    //     pv[i]=1;
 
-        for(auto x:a[i]){
-            if(!v[x]){
-                if(dfs(x,a,v,ans,pv)==0)
-                return 0;
-            }else if(pv[x]==1){
-                return 0;
-            }
-        }
-        pv[i]=0;
-        ans.push_back(i);
-        return 1;
-    }
+    //     for(auto x:a[i]){
+    //         if(!v[x]){
+    //             if(dfs(x,a,v,ans,pv)==0)
+    //             return 0;
+    //         }else if(pv[x]==1){
+    //             return 0;
+    //         }
+    //     }
+    //     pv[i]=0;
+    //     ans.push_back(i);
+    //     return 1;
+    // }
 
 
-    vector<int> eventualSafeNodes(vector<vector<int>>& a) {
-        int n=a.size();
+    vector<int> eventualSafeNodes(vector<vector<int>>& aa) {
+        int n=aa.size();
         vector<int>v(n,0);
-        vector<int>pv(n,0);
+        vector<int>d(n,0);
         vector<int>ans;
 
+        vector<vector<int>>a(n,vector<int>(0));
 
         for(int i=0;i<n;i++){
-            if(!v[i]){
-                dfs(i,a,v,ans,pv);
+            for(auto x:aa[i]){
+                a[x].push_back(i);
             }
         }
+
+        
+        for(int i=0;i<n;i++)
+        for(auto y:a[i])
+        d[y]++;
+        
+        queue<int>q;
+        
+        for(int i=0;i<n;i++){
+            if(!d[i])
+            q.push(i);
+        }
+        
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            
+            for(auto x:a[curr]){
+                d[x]--;
+                if(d[x]==0) q.push(x);
+            }
+            
+            ans.push_back(curr);
+            
+        }
+
         sort(ans.begin(),ans.end());
+
         return ans;
     }
 };
