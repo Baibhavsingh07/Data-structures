8a13b0669b0f3f21deb2471c187437fe5e0fc1ba
Baibhav Singh <baibhavsingh22@gmail.com>
2024-02-17 22:46:50 +0530

    Time: 283 ms (40.01%) | Memory: 19.8 MB (33.44%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int ladderLength(string bg, string eg, vector<string>& w) {
+        vector<vector<int>>a;
+
+        vector<int>v(w.size(),0);
+        queue<pair<string,int>>q;
+
+        q.push({bg,1});
+
+        set<string>set;
+
+        for(auto x:w) set.insert(x);
+
+        if(set.find(eg)==set.end()) return 0;
+
+        while(!q.empty()){
+            auto curr = q.front().first;
+            auto dis = q.front().second;
+            
+            q.pop();
+
+            
+
+            if(curr==eg) return dis;
+
+            for(int i=0;i<curr.size();i++){
+                for(int j=0;j<27;j++){
+                    if(curr[i]!='a'+j){
+                        char u = curr[i];
+                        curr[i]='a'+j;
+                        if(set.find(curr)!=set.end()){
+                            q.push({curr,dis+1});
+                            set.erase(curr);
+                        }
+
+                        curr[i]=u;
+                    }
+                }
+            }
+
+        }
+            return 0;
+
+    }
+};
