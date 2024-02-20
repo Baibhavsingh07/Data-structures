8286a5448e3331889f3ad0cbcf70aa45d4b904e9
Baibhav Singh <baibhavsingh22@gmail.com>
2024-02-20 22:43:48 +0530

    Time: 77 ms (89.03%) | Memory: 46.2 MB (44.71%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,96 @@
+class DisjointSet
+{
+public:
+
+    vector<int> rank, parent, size;
+
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& aa) {
+        int i,j;
+
+
+        DisjointSet ds(n);
+        int c=0;
+
+    for(auto x:aa){
+        if(ds.findUPar(x[0])==ds.findUPar(x[1]))
+        c++;
+        else
+        ds.unionBySize(x[0],x[1]);
+        
+    }
+
+    int s=0;
+
+    for(i=0;i<n;i++){
+        if(ds.parent[i]==i)s++;
+    }
+
+    s--;
+
+    if(c>=s) return s;
+    return -1;
+
+    }
+};
