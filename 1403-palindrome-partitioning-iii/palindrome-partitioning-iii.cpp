61904ba3d72d93d2c2521e7910ce4b64792df29a
Baibhav Singh <baibhavsingh22@gmail.com>
2024-03-06 22:00:14 +0530

    Time: 20 ms (36.30%) | Memory: 7.7 MB (72.60%) - LeetSync

--- 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.cpp
+++ 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-map<vector<int>,int>tbl;
+vector<vector<int>>tbl;
 
     int topal(int i,int j,string &s){
         int c=0;
@@ -16,20 +16,22 @@ map<vector<int>,int>tbl;
     int f(int i,string &s,int k){
         if(i==s.size() and k==0) return 0; 
         if(i==s.size()) return 1e9; 
+        if(k==0) return 1e9; 
 
         int ans=INT_MAX;
 
-        if(tbl.find({i,k})!=tbl.end()) return tbl[{i,k}];
+        if(tbl[i][k]!=-1) return tbl[i][k];
 
         for(int j=i;j<s.size();j++){
             ans=min(ans, topal(i,j,s) + f(j+1,s,k-1));
         }
 
-        return tbl[{i,k}] = ans;
+        return tbl[i][k] = ans;
 
     }
 
     int palindromePartition(string s, int k) {
+        tbl.resize(s.size()+1,vector<int>(k+1,-1));
         return f(0,s,k);
     }
 };
