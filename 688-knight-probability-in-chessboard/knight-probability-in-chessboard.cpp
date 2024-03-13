0fc804de38abb7cbfef0deb90768e83ab09fa9d7
Baibhav Singh <baibhavsingh22@gmail.com>
2024-03-13 17:55:27 +0530

    Time: 10 ms (49.95%) | Memory: 10.4 MB (43.55%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
 
-map<vector<long long>,double>tbl;
+// map<vector<long long>,double>tbl;
+vector<vector<vector<double>>>tbl;
 
     double f(int i, int j, int n, int k){
         if(i<0 or i>=n or j<0 or j>=n) return 0;
@@ -9,7 +10,7 @@ map<vector<long long>,double>tbl;
 
         double ans=0;
 
-        if(tbl.find({i,j,k})!=tbl.end()) return tbl[{i,j,k}];
+        if(tbl[i][j][k]!=-1) return tbl[i][j][k];
 
         int dx[8] = {1,1,-1,-1,2,2,-2,-2};
         int dy[8] = {2,-2,2,-2,1,-1,1,-1};
@@ -18,13 +19,14 @@ map<vector<long long>,double>tbl;
             ans=ans + (f(i+dx[x],j+dy[x],n,k-1))/8;
         }
 
-        return tbl[{i,j,k}] = ans;
+        return tbl[i][j][k] = ans;
     }
 
 
     double knightProbability(int n, int k, int i, int j) {
         // double x = (f(i,j,n,k))/1.0;
         // cout<<x;
+        tbl.resize(n+1,vector<vector<double>>(n+1,vector<double>(k+1,-1)));
         return (f(i,j,n,k));
         
     }
