1dee19a49f28ff80e7246c6b0b969d7b203059a5
Baibhav Singh <baibhavsingh22@gmail.com>
2024-03-28 14:49:34 +0530

    Time: 14 ms (31.54%) | Memory: 13.9 MB (39.64%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,54 +1,44 @@
 class Solution {
 public:
+
+    void dfs(int i,int j,vector<vector<char>>& a,vector<vector<int>>& v){
+        v[i][j]=1;
+
+        int d[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        for(auto val:d){
+            int x = i+val[0];
+            int y = j+val[1];
+
+            if(x>=0 and y>=0 and x<a.size() and y<a[0].size() and v[x][y]==0 and a[x][y]=='O')
+            dfs(x,y,a,v);
+
+        }
+        
+    }
+
     void solve(vector<vector<char>>& a) {
         int n=a.size(),m=a[0].size();
+        int i,j;
         vector<vector<int>>v(n,vector<int>(m,0));
 
-        queue<pair<int,int>>q;
-        for(int i=0;i<n;i++){
-            if(a[i][m-1]=='O'){
-                v[i][m-1]=1;
-                q.push({i,m-1});
-            }
-            if(a[i][0]=='O'){
-                v[i][0]=1;
-                q.push({i,0});
-            }
+        for(i=0;i<m;i++){
+            if(a[0][i]=='O' and !v[0][i]) dfs(0,i,a,v);
+        }
+        
+        for(i=0;i<m;i++){
+            if(a[n-1][i]=='O' and !v[n-1][i]) dfs(n-1,i,a,v);
+        }
+        
+        for(i=0;i<n;i++){
+            if(a[i][0]=='O' and !v[i][0]) dfs(i,0,a,v);
+        }
+        
+        for(i=0;i<n;i++){
+            if(a[i][m-1]=='O' and !v[i][m-1]) dfs(i,m-1,a,v);
         }
 
-        for(int i=0;i<m;i++){
-            if(a[0][i]=='O'){
-                v[0][i]=1;
-                q.push({0,i});
-            }
-            if(a[n-1][i]=='O'){
-                v[n-1][i]=1;
-                q.push({n-1,i});
-            }
-        }
+        for(i=0;i<n;i++) for(j=0;j<m;j++) if(v[i][j]==0) a[i][j]='X'; 
 
-        while(!q.empty()){
-            auto x = q.front();
-            q.pop();
-
-            int i=x.first,j=x.second;
-
-            int dx[4]={0,0,1,-1};
-            int dy[4]={1,-1,0,0};
-
-            for(int k = 0;k<4;k++){
-                if(i+dx[k]>=0 and i+dx[k]<n and j+dy[k]>=0 and j+dy[k]<m and v[i+dx[k]][j+dy[k]]==0 and a[i+dx[k]][j+dy[k]]=='O' ){
-                    v[i+dx[k]][j+dy[k]]=1;
-                    q.push({i+dx[k],j+dy[k]});
-                }
-            }
-        }
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(v[i][j]==0) a[i][j]='X';
-                else a[i][j]='O';
-            }
-        }
     }
 };
