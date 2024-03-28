7dd8076f8bc13c01ef537aaae0f3c611cee352e0
Baibhav Singh <baibhavsingh22@gmail.com>
2024-03-28 21:22:20 +0530

    Time: 323 ms (5.01%) | Memory: 115.7 MB (5.02%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -1,41 +1,32 @@
-#include <vector>
-#include <queue>
-#include <set>
-
-using namespace std;
-
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& a, vector<int>& x) {
-        int n = a.size(), m = a[0].size();
-        queue<vector<int>> q;
+        int n=a.size(),m=a[0].size();
+        queue<vector<int>>q;
         q.push(x);
 
+        set<vector<int>>v;
+        v.insert(x);
         a[x[0]][x[1]]='+';
 
-        set<vector<int>> v;
-        v.insert(x);
 
-        int dis = 1;
-        vector<vector<int>> directions = {{0,1},{0,-1},{1,0},{-1,0}};
-
-        while (!q.empty()) {
-            int size = q.size();
-            for (int i = 0; i < size; ++i) {
-                auto node = q.front();
+        int dis=1;
+        while(!q.empty()){
+            int l = q.size();
+            while(l--){
+                auto node= q.front();
                 q.pop();
 
-                for (const auto& d : directions) {
-                    int nx = node[0] + d[0];
-                    int ny = node[1] + d[1];
+                vector<vector<int>>d = {{0,1},{0,-1},{1,0},{-1,0}};
 
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && a[nx][ny] == '.') {
-                        if (nx == 0 || ny == 0 || nx == n - 1 || ny == m - 1) return dis;
+                for(auto vv:d){
+                    int nx = node[0]+vv[0];
+                    int ny = node[1]+vv[1];
+                    if(nx>=0 and ny>=0 and nx<n and ny<m and a[nx][ny]=='.'){
+                        if(nx==0 or ny==0 or nx==n-1 or ny==m-1) return dis;
 
-                        if (v.find({nx, ny}) == v.end()) {
-                            q.push({nx, ny});
-                            v.insert({nx, ny});
-                        }
+                        q.push({nx,ny});
+                        a[nx][ny]='+';
                     }
                 }
             }
