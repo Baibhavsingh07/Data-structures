0369c263cf7c3accf5846b1df0777ed8b489d716
Baibhav Singh <baibhavsingh22@gmail.com>
2024-03-28 14:16:35 +0530

    Time: 160 ms (16.29%) | Memory: 65.1 MB (33.15%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,65 +1,38 @@
 class Solution {
 public:
-
-    // int dfs(int i,vector<vector<int>>&a,vector<int>&v,vector<int>&ans,vector<int>&pv){
-    //     v[i]=1;
-    //     pv[i]=1;
-
-    //     for(auto x:a[i]){
-    //         if(!v[x]){
-    //             if(dfs(x,a,v,ans,pv)==0)
-    //             return 0;
-    //         }else if(pv[x]==1){
-    //             return 0;
-    //         }
-    //     }
-    //     pv[i]=0;
-    //     ans.push_back(i);
-    //     return 1;
-    // }
-
-
     vector<int> eventualSafeNodes(vector<vector<int>>& aa) {
         int n=aa.size();
-        vector<int>v(n,0);
-        vector<int>d(n,0);
-        vector<int>ans;
-
         vector<vector<int>>a(n,vector<int>(0));
 
-        for(int i=0;i<n;i++){
+        vector<int>d(n,0);
+        vector<int>safe;
+
+        for(int i=0;i<aa.size();i++){
             for(auto x:aa[i]){
                 a[x].push_back(i);
+                d[i]++;
             }
         }
 
-        
-        for(int i=0;i<n;i++)
-        for(auto y:a[i])
-        d[y]++;
-        
         queue<int>q;
-        
-        for(int i=0;i<n;i++){
-            if(!d[i])
-            q.push(i);
-        }
-        
+
+        for(int i=0;i<n;i++) if(d[i]==0) q.push(i);
+
         while(!q.empty()){
-            int curr = q.front();
+            auto x = q.front();
             q.pop();
-            
-            for(auto x:a[curr]){
-                d[x]--;
-                if(d[x]==0) q.push(x);
+            safe.push_back(x);
+
+            for(auto y:a[x]){
+                d[y]--;
+                if(d[y]==0) q.push(y);
             }
-            
-            ans.push_back(curr);
-            
+
         }
 
-        sort(ans.begin(),ans.end());
+        sort(safe.begin(),safe.end());
+        
+        return safe;
 
-        return ans;
     }
 };
