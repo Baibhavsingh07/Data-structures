a9e750936da4b00a7fea34fb2c8740d841d1af4b
Baibhav Singh <baibhavsingh22@gmail.com>
2024-04-04 12:09:44 +0530

    Time: 127 ms (78.77%) | Memory: 91.3 MB (34.96%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -1,15 +1,42 @@
 class Solution {
 public:
     int maximumGap(vector<int>& a) {
-        sort(a.begin(),a.end());
+        int i,j;
+        int c=0,n=a.size();
 
-        int i,j,c=0;
+        if(n==1) return 0;
+        if(n==2) return abs(a[0]-a[1]);
 
-        for(i=1;i<a.size();i++){
-            if(a[i]-a[i-1]>c) c=a[i]-a[i-1];
+        int ma=*max_element(a.begin(),a.end());
+        int mi=*min_element(a.begin(),a.end());
+
+        int gap = (int)ceil((ma-mi+0.0)/(n-1));
+        if(ma==mi) gap=1;
+
+        vector<int>mib(n-1,INT_MAX);  // min bucket
+        vector<int>mab(n-1,INT_MIN);  //max_bucket
+
+        for(i=0;i<n;i++){
+            if(a[i]==mi or a[i]==ma) continue;
+
+            int buck_no = (a[i]-mi)/gap;
+            mib[buck_no] = min(mib[buck_no],a[i]);
+            mab[buck_no] = max(mab[buck_no],a[i]);
         }
 
-        return c;
+
+        int ans=0;
+        int prev=mi;
+        for(i=0;i<mib.size();i++){
+            if(mib[i]!=INT_MAX ){   //if bucket is not empty
+               ans=max(ans,mib[i]-prev);  
+                prev=mab[i];
+            }
+                
+            
+        }
+
+        return max(ans,ma-prev);
 
     }
 };
