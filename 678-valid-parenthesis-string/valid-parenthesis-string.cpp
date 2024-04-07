3438be6d903990cecffb004a2f0b05928c9c15bd
Baibhav Singh <baibhavsingh22@gmail.com>
2024-04-07 10:51:49 +0530

    Time: 15 ms (5.26%) | Memory: 27.7 MB (7.00%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-map<vector<int>,int>tbl;
+vector<vector<int>>tbl;
 
     int f(int i,string s,int bal){
         if(i==s.size() and bal==0) return 1;
@@ -9,15 +9,16 @@ map<vector<int>,int>tbl;
 
         if(bal<0) return 0; 
 
-        if(tbl.find({i,bal})!=tbl.end()) return tbl[{i,bal}];
+        if(tbl[i][s.size()+bal]!=-1) return tbl[i][s.size()+bal];
 
-        if(s[i]=='(') return tbl[{i,bal}] = f(i+1,s,bal+1);
-        if(s[i]==')') return tbl[{i,bal}] = f(i+1,s,bal-1);
-        else return tbl[{i,bal}] = f(i+1,s,bal) or f(i+1,s,bal-1) or f(i+1,s,bal+1); 
+        if(s[i]=='(') return tbl[i][s.size()+bal] = f(i+1,s,bal+1);
+        if(s[i]==')') return tbl[i][s.size()+bal] = f(i+1,s,bal-1);
+        else return tbl[i][s.size()+bal] = f(i+1,s,bal) or f(i+1,s,bal-1) or f(i+1,s,bal+1); 
     }
 
 
     bool checkValidString(string s) {
+        tbl.resize(s.size()+1,vector<int>(s.size()+s.size()+2,-1));
         return f(0,s,0);
     }
 };
