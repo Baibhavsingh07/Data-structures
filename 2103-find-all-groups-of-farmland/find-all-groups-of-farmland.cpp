20d658cd83539c8dae1e5b989cbc97a1cd5efa1f
Baibhav Singh <baibhavsingh22@gmail.com>
2024-04-20 09:31:03 +0530

    Time: 107 ms (64.73%) | Memory: 77.8 MB (45.09%) - LeetSync

---
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+    void dfs(int i,int j,vector<vector<int>>&a,vector<vector<int>>&v,int &li,int &lj,int &ri,int &rj){
+        v[i][j]=1;
+
+        int c=0;
+
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
+
+        for(int k=0;k<4;k++){
+            int x=i+dx[k];
+            int y=j+dy[k];
+
+            if(x>=0 and y>=0 and x<a.size() and y<a[0].size() and v[x][y]==0 and a[x][y]==1){
+                li=min(li,x);
+                ri=max(ri,x);
+                lj=min(lj,y);
+                rj=max(rj,y);
+                dfs(x,y,a,v,li,lj,ri,rj);
+            }
+        }
+
+        
+
+
+    }
+
+    vector<vector<int>> findFarmland(vector<vector<int>>& a) {
+        vector<vector<int>>ans;
+
+        int i,j,c=0,s=0;
+        vector<vector<int>>v(a.size(),vector<int>(a[0].size(),0));
+
+        for(i=0;i<a.size();i++){
+            for(j=0;j<a[0].size();j++){
+                if(!v[i][j] and a[i][j]==1){
+                    int li=i,lj=j,ri=i,rj=j;
+
+                    dfs(i,j,a,v,li,lj,ri,rj);
+
+                    ans.push_back({li,lj,ri,rj});
+                }
+            }
+        }
+
+        return ans;
+
+    }
+};
