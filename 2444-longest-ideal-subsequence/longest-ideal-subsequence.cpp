972ac22d92bad2d04d0f2e71021f0a92a79a5cd4
Baibhav Singh <baibhavsingh22@gmail.com>
2024-04-25 11:27:38 +0530

    Time: 571 ms (14.28%) | Memory: 456 MB (10.08%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -6,7 +6,7 @@ vector<vector<int>>tbl;
     int f(int i,char p,string &s,int k){
         if(i==s.size()) return 0;
 
-        if(tbl[i][p+1-'a']!=-1) return tbl[i][p+1-'a'];
+        if(tbl[i][p]!=-1) return tbl[i][p];
 
         int l = f(i+1,p,s,k);
         int r=0;
@@ -14,11 +14,11 @@ vector<vector<int>>tbl;
         if(p==char(96) or abs(s[i]-p)<=k)
         r = 1 + f(i+1,s[i],s,k);
 
-        return tbl[i][p+1-'a']= max(l,r);
+        return tbl[i][p]= max(l,r);
     }
 
     int longestIdealString(string s, int k) {
-        tbl.resize(s.size(),vector<int>(27,-1));
+        tbl.resize(s.size(),vector<int>(123,-1));
         return f(0,char(96),s,k);
     }
 };
