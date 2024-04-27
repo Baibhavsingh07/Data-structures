2b8d32f0dc55e26f4810081a6751bdeec0d7b76e
Baibhav Singh <baibhavsingh22@gmail.com>
2024-04-27 08:58:50 +0530

    Time: 850 ms (5.19%) | Memory: 392.4 MB (6.37%) - LeetSync

---
+++ 514-freedom-trail/freedom-trail.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+int x;
+
+vector<vector<vector<int>>>tbl;
+map<char,vector<int>>map;
+
+    int f(int i,int j,int n,string &ring, string &key){
+        if(j==key.size()) return 0;
+        if(n<0) return INT_MAX/2;
+
+        string s = to_string(i)+'-'+to_string(j)+'-'+to_string(n);
+
+        if(tbl[i][j][n]!=-1) return tbl[i][j][n];
+
+        int ans=INT_MAX;
+        if(ring[i]==key[j]){
+            ans=1 + f(i,j+1,ring.size()/2,ring,key);
+        }else{
+            auto vec = map[key[j]];
+
+            for(auto val:vec){
+                int d = min(abs(val-i),x-abs(val-i));
+
+                ans = min(ans,d+f(val,j,n,ring ,key));
+            }
+        }
+
+        return tbl[i][j][n] =ans;
+    }
+
+    int findRotateSteps(string ring, string key) {
+        x=ring.size();
+
+        tbl.resize(101,vector<vector<int>>(101,vector<int>(101,-1)));
+
+        for(int i=0;i<ring.size();i++)map[ring[i]].push_back(i);
+
+        return f(0,0,ring.size(),ring,key);
+    }
+};
