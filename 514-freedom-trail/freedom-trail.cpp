24680b701e1a25a8ab3a93bdaaf82eac89b06ad9
Baibhav Singh <baibhavsingh22@gmail.com>
2024-04-27 09:00:24 +0530

    Time: 35 ms (41.04%) | Memory: 36.4 MB (25.71%) - LeetSync

--- 514-freedom-trail/freedom-trail.cpp
+++ 514-freedom-trail/freedom-trail.cpp
@@ -6,24 +6,22 @@ int x;
 vector<vector<int>>tbl;
 map<char,vector<int>>map;
 
-    int f(int i,int j,int n,string &ring, string &key){
+    int f(int i,int j,string &ring, string &key){
         if(j==key.size()) return 0;
-        if(n<0) return INT_MAX/2;
 
-        string s = to_string(i)+'-'+to_string(j)+'-'+to_string(n);
 
         if(tbl[i][j]!=-1) return tbl[i][j];
 
         int ans=INT_MAX;
         if(ring[i]==key[j]){
-            ans=1 + f(i,j+1,ring.size()/2,ring,key);
+            ans=1 + f(i,j+1,ring,key);
         }else{
             auto vec = map[key[j]];
 
             for(auto val:vec){
                 int d = min(abs(val-i),x-abs(val-i));
 
-                ans = min(ans,d+f(val,j,n,ring ,key));
+                ans = min(ans,d+f(val,j,ring ,key));
             }
         }
 
@@ -37,6 +35,6 @@ map<char,vector<int>>map;
 
         for(int i=0;i<ring.size();i++)map[ring[i]].push_back(i);
 
-        return f(0,0,ring.size(),ring,key);
+        return f(0,0,ring,key);
     }
 };
