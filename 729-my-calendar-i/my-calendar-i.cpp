04b27be7ce147bcb01e088f1025ba0a4d788f338
Baibhav Singh <baibhavsingh22@gmail.com>
2024-04-29 14:08:54 +0530

    Time: 72 ms (71.80%) | Memory: 42.4 MB (64.17%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -0,0 +1,31 @@
+class MyCalendar {
+public:
+map<int,int>events;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        auto event = events.lower_bound(start);
+        if(event!=events.end() ) {
+            // if(preevent->second>start) return 0;
+            if(max(start,event->first) < min(end,event->second)) return 0;
+        }
+
+
+        if(event!=events.begin() ){
+            auto preevent = prev(event);
+            // if(preevent->second>start) return 0;
+            if(max(start,preevent->first) < min(end,preevent->second)) return 0;
+        }
+
+        events[start]=end;
+        return 1;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
