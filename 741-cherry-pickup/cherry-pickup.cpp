95dfab5ec78bcad28570f0d7f9521e08128d0ef3
Baibhav Singh <baibhavsingh22@gmail.com>
2024-04-30 17:46:39 +0530

    Time: 51 ms (50.48%) | Memory: 25.8 MB (59.40%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -4,7 +4,7 @@ public:
 vector<vector<vector<int>>>tbl;
 
     int f(int i,int j,int k,int l,vector<vector<int>>& a){
-        if(i==a.size()-1 and j==a[0].size()-1) return tbl[i][j][k] = a[i][j];
+        if(i==a.size()-1 and j==a[0].size()-1 and k==a.size()-1 and l==a[0].size()-1) return tbl[i][j][k] = a[i][j];
         if(i==a.size() or j==a[0].size() or k==a.size() or l==a[0].size() ) return INT_MIN;
         if(a[i][j]==-1 or a[k][l]==-1) return INT_MIN;
 
@@ -22,7 +22,7 @@ vector<vector<vector<int>>>tbl;
 
 
     int cherryPickup(vector<vector<int>>& a) {
-        tbl.resize(a.size()+1,vector<vector<int>>(a[0].size()+1,vector<int>(a.size()+1,-1)));
+        tbl.resize(a.size(),vector<vector<int>>(a[0].size(),vector<int>(a.size(),-1)));
         return f(0,0,0,0,a)<0?0:f(0,0,0,0,a);
     }
 };
