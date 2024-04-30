a94780b64f06514f9493aaf1a26f99ffeccb0433
Baibhav Singh <baibhavsingh22@gmail.com>
2024-04-30 17:51:51 +0530

    Time: 367 ms (25.79%) | Memory: 136.5 MB (24.14%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
 
-vector<vector<vector<int>>>tbl;
+unordered_map<string,int>tbl;
 
     int f(int i,int j,int k,int l,vector<vector<int>>& a){
-        if(i==a.size()-1 and j==a[0].size()-1 and k==a.size()-1 and l==a[0].size()-1) return tbl[i][j][k] = a[i][j];
-        if(i==a.size() or j==a[0].size() or k==a.size() or l==a[0].size() ) return INT_MIN;
-        if(a[i][j]==-1 or a[k][l]==-1) return INT_MIN;
+        if(i==a.size()-1 and j==a[0].size()-1 and k==a.size()-1 and l==a[0].size()-1)                   
+            return a[i][j];
+            
+        if(i==a.size() or j==a[0].size() or k==a.size() or l==a[0].size() ) 
+            return INT_MIN;
 
-        if(tbl[i][j][k]!=-1) return tbl[i][j][k];
+
+        if(a[i][j]==-1 or a[k][l]==-1) 
+            return INT_MIN;
+
+        string key = to_string(i)+'-'+to_string(j)+'-'+to_string(k);
+
+        if(tbl.find(key)!=tbl.end()) return tbl[key];
 
         int ans=a[i][j];
         if(i!=k)ans+=a[k][l];
@@ -17,12 +25,11 @@ vector<vector<vector<int>>>tbl;
 
         // if(a[i][j]==1) a[i][j]=0;
 
-        return tbl[i][j][k] = ans;
+        return tbl[key] = ans;
     }
 
 
     int cherryPickup(vector<vector<int>>& a) {
-        tbl.resize(a.size(),vector<vector<int>>(a[0].size(),vector<int>(a.size(),-1)));
         return f(0,0,0,0,a)<0?0:f(0,0,0,0,a);
     }
 };
