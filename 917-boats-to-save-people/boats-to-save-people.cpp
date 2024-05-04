35a6cc6b87452565b37899f144a503c67d00f727
Baibhav Singh <baibhavsingh22@gmail.com>
2024-05-04 09:22:09 +0530

    Time: 50 ms (89.23%) | Memory: 45.4 MB (85.44%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    int numRescueBoats(vector<int>& a, int l) {
-        sort(a.begin(),a.end());
+    int numRescueBoats(vector<int>& people, int limit) {
+        // Sort the array to simplify the boat allocation process
+        sort(people.begin(), people.end());
 
-        int i=0,j=a.size()-1,c=0;
+        // Initialize two pointers, one at the beginning and one at the end of the array
+        int left = 0; // Pointer for the lightest person
+        int right = people.size() - 1; // Pointer for the heaviest person
+        int boatsCount = 0; // Count of boats required
 
-        while(i<=j){
-            if(a[i]+a[j]<=l){
-                c++;
-                i++;
-                j--;
-            }else if(a[j]<=l){
-                c++;
-                j--;
+        // Iterate until both pointers meet or cross each other
+        while (left <= right) {
+            // If both the lightest and heaviest persons can fit in the same boat
+            if (people[left] + people[right] <= limit) {
+                boatsCount++; // Allocate a boat
+                left++; // Move to the next lightest person
+                right--; // Move to the next heaviest person
+            } 
+            // If only the heaviest person can fit in a boat
+            else if (people[right] <= limit) {
+                boatsCount++; // Allocate a boat
+                right--; // Move to the next heaviest person
             }
         }
 
-        return c;
+        return boatsCount; // Return the total count of boats required
     }
 };
