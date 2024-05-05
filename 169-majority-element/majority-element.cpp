5175d52e6a7bd0c4e9da58894d6f1f8f5c339521
Baibhav Singh <baibhavsingh22@gmail.com>
2024-05-05 22:41:01 +0530

    Time: 20 ms (39.46%) | Memory: 26.7 MB (48.75%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& a) {
+        int i,j,c=0,s=0;
+        int m=a[0],mc=1;
+
+        for(i=1;i<a.size();i++){
+            if(m!=a[i] and mc==0){
+                m=a[i];
+                mc=1;
+            }else if(m==a[i]){
+                mc++;
+            }else {
+                mc--;
+            }
+        }
+
+        return m;
+    }
+};
