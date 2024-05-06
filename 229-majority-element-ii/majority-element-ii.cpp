9b7f5555abaf493c1781cfe006335741671512b7
Baibhav Singh <baibhavsingh22@gmail.com>
2024-05-06 21:23:02 +0530

    Time: 3 ms (98.48%) | Memory: 18.3 MB (91.76%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,15 +1,46 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& a) {
-        unordered_map<int,int>m;
-        vector<int>ans;
-        int k = a.size()/3;
+        if(a.size()<2) return a;
 
-        for(auto x:a)m[x]++;
+        int m1,m2,mc1=0,mc2=0,i;
 
-        for(auto x:m)if(x.second>k) ans.push_back(x.first);
+        for(i=0;i<a.size();i++){
+            if(m2!=a[i] and mc1==0 ){
+                m1=a[i];
+                mc1=1;
+            }else if(m1!=a[i] and mc2==0){
+                m2=a[i];
+                mc2=1;
+            }else if(m1==a[i]){
+                mc1++;
+            }else if(m2==a[i]){
+                mc2++;
+            }else{
+                mc2--;
+                mc1--;
+            }
 
-        return ans;
+        }
+
+        int c1=0,c2=0;
+
+        for(auto x:a){
+            if(x==m1) c1++;
+            else if(x==m2) c2++;
+        }
+
+        int n=a.size();
+
+        if(c1> n/3 and c2> n/3){ 
+             if(m1==m2) return {m1};
+            else return {m1,m2};
+        }
+
+        if(c1>n/3) return {m1}; 
+        if(c2>n/3) return {m2}; 
+
+        return {};
 
     }
 };
