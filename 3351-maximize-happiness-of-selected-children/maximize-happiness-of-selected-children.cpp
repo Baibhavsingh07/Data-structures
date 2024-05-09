783fe70d73690d7edeb7ad68c85eb803efa74ecf
Baibhav Singh <baibhavsingh22@gmail.com>
2024-05-09 10:40:40 +0530

    Time: 156 ms (73.69%) | Memory: 107 MB (98.04%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -4,16 +4,13 @@ public:
         long long ans=0;
         int i,j,c=0,s=0;
 
-        multiset<int>set;
+        sort(a.begin(),a.end(),greater<int>());
 
-        for(auto x:a)set.insert(x);
-
-        while(k-- and !set.empty()){
-            auto x = *set.rbegin();
-            set.erase(prev(set.end()));
-
-            if(x-c >0)
-            ans = ans + x-c;
+        for(i=0;i<k;i++){
+            if(a[i]-c>0){
+                ans+=a[i]-c;
+                
+            }
             c++;
         }
 
