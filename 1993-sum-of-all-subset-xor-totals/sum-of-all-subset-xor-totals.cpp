c9036f8271f2b6e1d7075c81465a7f9677f2eace
Baibhav Singh <baibhavsingh22@gmail.com>
2024-05-20 14:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (98.56%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -3,25 +3,18 @@ public:
 
     vector<int>ans;
 
-    void f(int i,vector<int>& a,int curr){
+    int f(int i,vector<int>& a,int x){
         if(i==a.size()) {
-            ans.push_back(curr);
-            return;
+            return x;
         }
 
-        f(i+1,a,curr);
+        auto l =f(i+1,a,x);
+        auto r = f(i+1,a,x^a[i]);
 
-        int x=curr;
-        curr^=a[i];
-        f(i+1,a,curr);
-        curr=x;
+        return l +   r;
     }
 
     int subsetXORSum(vector<int>& a) {
-        int curr=0;
-
-        f(0,a,curr);
-
-        return accumulate(ans.begin(),ans.end(),0);
+        return f(0,a,0);
     }
 };
