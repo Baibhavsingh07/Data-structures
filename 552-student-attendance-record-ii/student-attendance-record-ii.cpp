dee2d7dba8ab5977132995d6758d2e7e8d259f2e
Baibhav Singh <baibhavsingh22@gmail.com>
2024-05-26 10:43:49 +0530

    Time: 207 ms (59.47%) | Memory: 20.1 MB (67.70%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
 
+    int tbl[100001][4][3];
 
-    int f(int i,int n,int lc,int ac,vector<vector<vector<int>>>&tbl){
+    int f(int i,int n,int lc,int ac){
         if(i==n) return 1;
 
         long long r=0,m=0;
 
         if(tbl[i][lc][ac]!=-1) return tbl[i][lc][ac];
 
-        auto l = f(i+1,n,0,ac,tbl)%1000000007;
+        auto l = f(i+1,n,0,ac)%1000000007;
         if(lc<2)
-         r = f(i+1,n,lc+1,ac,tbl)%1000000007;
+         r = f(i+1,n,lc+1,ac)%1000000007;
 
         if(ac>0)
-         m = f(i+1,n,0,ac-1,tbl)%1000000007;
+         m = f(i+1,n,0,ac-1)%1000000007;
 
         return tbl[i][lc][ac]= (l+r+m)%1000000007;
 
     }
 
     int checkRecord(int n) {
-            vector<vector<vector<int>>>tbl(n+1,vector<vector<int>>(4,vector<int>(2,-1)));
-        return f(0,n,0,1,tbl);
+        for(int i=0;i<100001;i++) for(int j=0;j<4;j++) for(int k=0;k<3;k++) tbl[i][j][k]=-1;
+        return f(0,n,0,1);
     }
 };
