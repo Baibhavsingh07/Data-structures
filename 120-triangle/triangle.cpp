233651a8ccf64388e4b615356c9a3ee610c82b95
Baibhav Singh <baibhavsingh22@gmail.com>
2024-05-28 16:13:41 +0530

    Time: 6 ms (35.03%) | Memory: 11.4 MB (52.98%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
 
-    map<vector<int>,int>tbl;
+    vector<vector<int>>tbl;
 
     int f(int i,int j,vector<vector<int>>& a,int n){
         if(i==n) return 0;
 
-        if(tbl.find({i,j})!=tbl.end()) return tbl[{i,j}];
+        if(tbl[i][j]!=INT_MAX) return tbl[i][j];
 
         auto l = a[i][j] +f(i+1,j,a,n);
         auto r = a[i][j]+ f(i+1,j+1,a,n);
 
-        return tbl[{i,j}]= min(l,r);
+        return tbl[i][j]= min(l,r);
     }
 
     int minimumTotal(vector<vector<int>>& a) {
+        tbl.resize(a.size(),vector<int>(a.size()+1,INT_MAX));
         return f(0,0,a,a.size());
     }
 };
