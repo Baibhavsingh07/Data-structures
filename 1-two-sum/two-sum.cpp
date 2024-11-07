444ead8121d10618ebec35647ea707d1c4641fc8
Baibhav Singh <baibhavsingh22@gmail.com>
2024-11-07 19:40:09 +0530

    Time: 40 ms (44.55%) | Memory: 12.8 MB (89.40%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,39 +1,19 @@
 class Solution {
-    private:
-    int search(vector<int>a,int t){
-        for(int i=0;i<a.size();i++)
-        if(a[i]==t) return i;
-
-        return -1;
-    }
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        // vector<int>ans;
+    vector<int> twoSum(vector<int>& a, int t) {
+        vector<int>ans(2);
+        int i,j,c=0,s=0;
 
-        // for(int i=0;i<nums.size();i++){
-        //     for(int j=i+1;j<nums.size();j++)
-        //     if(nums[i]+nums[j]==target) return {i,j};
-
-        // }
-        // return ans;
-
-        // for(int i=0;i<nums.size();i++){
-        //     int j = search(nums,target-nums[i]);
-
-        //     if(j!=-1 and i!=j) return {i,j};
-        // }
-
-        // return {-1,-1};
-
-        unordered_map<int,int>map;
-        for(int i=0;i<nums.size();i++){
-            if(map.find(target-nums[i])!=map.end()){
-                return {i,map[target-nums[i]]};
+        for(i=0;i<a.size();i++){
+            for(j=i+1;j<a.size();j++){
+                if(a[i]+a[j] == t) {
+                    ans[0]=i;
+                    ans[1]=j;
+                    return ans;
+                }
             }
-            map[nums[i]]=i;
         }
 
         return {-1,-1};
-        
     }
 };
